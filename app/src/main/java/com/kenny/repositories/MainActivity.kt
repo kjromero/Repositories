3f81833f6d6465f55d4918e06d4625e7336772a8
Kenny Romero <sastoque.kenny@gmail.com>
2022-12-06 10:07:44 -0500

    Initial commit

---
+++ app/src/main/java/com/kenny/repositories/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kenny.repositories
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
